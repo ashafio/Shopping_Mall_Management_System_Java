bc3b1502b83ae917bdfc4bbb9ce5ddf4a1367713
MARShafi <abidurrahman780@gmail.com>
2021-10-12 22:44:18 +0600

    Initial Upload

---
+++ src/OOP_CSE_2214/Buyer.java
@@ -0,0 +1,4 @@
+package OOP_CSE_2214;
+
+public class Buyer {
+}
---
+++ src/OOP_CSE_2214/Main.java
@@ -0,0 +1,8 @@
+package OOP_CSE_2214;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
